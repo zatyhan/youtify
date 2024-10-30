2fa02a2d2da20988906bdb8dbbfd4c44af0d0f1d
zatyhan <nureizzaty@gmail.com>
2024-10-30 11:33:46 +0000

    test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,42 +2,45 @@ import streamlit as st
 from helpers import Processor, PlaylistMaker
 import webbrowser
 
-st.title("Youtify 🎵")
-st.write(
-    "Convert your favorite YouTube music videos to Spotify playlists!"
-)
+def main():
+    st.title("Youtify 🎵")
+    st.write(
+        "Convert your favorite YouTube music videos to Spotify playlists!"
+    )
+    
+    input_form= st.form(key='form', clear_on_submit=True)
+    # Get the YouTube URL from the user
+    youtube_url = input_form.text_input("Enter the YouTube URL:", key="youtube_url")
 
-input_form= st.form(key='form', clear_on_submit=True)
-# Get the YouTube URL from the user
-youtube_url = input_form.text_input("Enter the YouTube URL:", key="youtube_url")
+    # Get the name of the playlist from the user
+    playlist_name= input_form.text_input("Enter the name of the playlist you want to create on Spotify:", key="playlist_name")
 
-# Get the name of the playlist from the user
-playlist_name= input_form.text_input("Enter the name of the playlist you want to create on Spotify:", key="playlist_name")
+    button = input_form.form_submit_button('Create my playlist!')
 
-button = input_form.form_submit_button('Create my playlist!')
+    if button:
+        playlist= PlaylistMaker(st.session_state.playlist_name)
 
-if button:
-    playlist= PlaylistMaker(st.session_state.playlist_name)
+        if 'sp' not in st.session_state:
+            auth_url = playlist.authenticate()
+            # webbrowser.open(url=st.session_state.auth_url)
+            st.markdown(f'<meta http-equiv="refresh" content="0; url={auth_url}">', unsafe_allow_html=True)
+            print('User has been successfully authenticated')
+            print(st.query_params)
 
-    if 'sp' not in st.session_state:
-        st.session_state.auth_url = playlist.authenticate()
-        webbrowser.open(url=st.session_state.auth_url)
-        print('User has been successfully authenticated')
-        print(st.query_params)
+        else: 
+            sp= st.session_state.sp
+            user_info = sp.me()
+            st.write(f"Hello {user_info['display_name']}!")
 
-    else: 
-        sp= st.session_state.sp
-        user_info = sp.me()
-        st.write(f"Hello {user_info['display_name']}!")
+        if 'code' in st.query_params:
+            code = st.query_params['code'][0]
+            playlist.create_playlist(code)
+            st.write('Successfully created playlist')
 
-    if 'code' in st.query_params:
-        code = st.query_params['code'][0]
-        playlist.create_playlist(code)
-        st.write('Successfully created playlist')
+        else: 
+            st.write('Something went wrong.')
+            raise SystemExit
 
-    else: 
-        st.write('Something went wrong.')
-        raise SystemExit
-
-
+if __name__ == "__main__":
+    main()
 
