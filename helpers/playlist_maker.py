ecc411487c46d83fd42948dc9b84665fe5db6b19
zatyhan <nureizzaty@gmail.com>
2024-10-30 10:32:12 +0000

    update to UI

--- helpers/playlist_maker.py
+++ helpers/playlist_maker.py
@@ -10,23 +10,30 @@ class PlaylistMaker():
     def __init__(self, name):
         load_dotenv()
         self.playlist_name = name
-        self.__clientID__ = os.getenv('SPOTIFY_CLIENT_ID')
+        self.__clientID__ = "d3eb6ef85c20439d89f4c6b100024b20"        # self.__clientID__ = os.getenv('SPOTIFY_CLIENT_ID')
         self.__scope__='playlist-modify-public'
         self.__auth_manager__ = SpotifyPKCE(client_id=self.__clientID__, redirect_uri="https://youtify.streamlit.app/", scope=self.__scope__)
+        code= self.__auth_manager__.get_authorization_code()
+        print(code)
         self.__sp__ = spotipy.Spotify(auth_manager=self.__auth_manager__)
         self.user_id= self.__sp__.me()['id']
         self.playlist_id = self.__sp__.user_playlist_create(self.user_id, self.playlist_name)['id'] #add template for description later
 
     def lookup(self, isrc):
-        query= f"isrc:{isrc}"
-        results = self.__sp__.search(query, type="track")
-        track_id= results['tracks']['items'][0]['id']
-        duration= results['tracks']['items'][0]['duration_ms']/1000
-        track_title= results['tracks']['items'][0]['name']
-        return track_id, track_title, duration
+        if  isrc:
+            query= f"isrc:{isrc}"
+            results = self.__sp__.search(query, type="track")
+            track_id= results['tracks']['items'][0]['id']
+            duration= results['tracks']['items'][0]['duration_ms']/1000
+            track_title= results['tracks']['items'][0]['name']
+            return track_id, track_title, duration
 
     def add_to_playlist(self, track_id):
         self.__sp__.user_playlist_add_tracks(self.user_id, self.playlist_id, [track_id])
 
     def get_playlist(self):
         return self.__sp__.playlist(self.playlist_id)
+
+pl = PlaylistMaker('test1')
+pl.add_to_playlist('6rqhFgbbKwnb9MLmUQDhG6')
+# https://open.spotify.com/track/pwd?si=31500007bb7144e6
