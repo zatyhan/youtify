27777f84d34dc71d7f0f2533b95b53a6179c1b81
zatyhan <nureizzaty@gmail.com>
2024-11-13 16:38:36 +0000

    created new function sign_in to refactor code

--- helpers/playlist_maker.py
+++ helpers/playlist_maker.py
@@ -8,23 +8,26 @@ from urllib.parse import urlencode
 import webbrowser
 
 class PlaylistMaker():
-    def __init__(self, name):
+    def __init__(self):
         load_dotenv()
-        self.playlist_name = name
+        self.playlist_name = " "
         self.__clientID__ = "d3eb6ef85c20439d89f4c6b100024b20"        
         # self.__clientID__ = os.getenv('SPOTIFY_CLIENT_ID')
         self.__scope__='playlist-modify-public'
         self.__auth_manager__= None
     
-    def authenticate(self):
+    def get_auth_url(self):
         self.__auth_manager__ = SpotifyPKCE(client_id=self.__clientID__, redirect_uri="https://organic-meme-7xpj5v76pjr3xq9j-8501.app.github.dev/", scope=self.__scope__)
         url= self.__auth_manager__.get_authorize_url()
         return url        
 
-    def create_playlist(self, code):
+    def authorize(self, code):
         self.__auth_manager__.get_access_token(code)
         self.__sp__ = spotipy.Spotify(auth_manager=self.__auth_manager__)
         self.user_id= self.__sp__.me()['id']
+
+    def create_playlist(self, name):
+        self.playlist_name= name
         self.playlist_id = self.__sp__.user_playlist_create(self.user_id, self.playlist_name)['id'] #add template for description later
         
     def lookup(self, isrc):
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from helpers import Processor, PlaylistMaker
 import asyncio
-
     
 @st.cache_data
 def get_cached_playlist():
@@ -14,53 +13,71 @@ def set_cached_playlist(playlist):
 # @st.cache_data
 # def authenticator():
 
+def sign_in():
+    try: 
+        auth_url= st.session_state.playlist.get_auth_url()
+        link_html = " <a href=\"{url}\" >{msg}</a> ".format(
+        url=auth_url,
+        msg="Click me to authenticate!")
+        st.markdown(link_html)
+    except Exception as e: 
+        st.write(f'Error during authorization: {str(e)}')
+    
+    else: 
+        st.session_state.signed_in= True
+        st.success('Successfully authenticated!')
 
-def main():
-    st.title("Youtify 🎵")
-    st.write(
-        "Convert your favorite YouTube music videos to Spotify playlists!"
-    )
 
-    if 'playlist' not in st.session_state:
-        st.session_state.playlist= get_cached_playlist()
+st.title("Youtify 🎵")
+st.write(
+    "Convert your favorite YouTube music videos to Spotify playlists!"
+)
 
-    input_form= st.form(key='form', clear_on_submit=True)
-    youtube_url = input_form.text_input("Enter the YouTube URL:", key="youtube_url", value= "https://www.youtube.com/watch?v=rMZeKQYLXWc")
-    playlist_name= input_form.text_input("Enter the name of the playlist you want to create on Spotify:", key="playlist_name", value= "My Playlist")
-    button = input_form.form_submit_button('Create my playlist!')
+if 'playlist' not in st.session_state:
+    st.session_state.playlist= get_cached_playlist()
 
-    if button:
-        st.session_state.playlist= PlaylistMaker(st.session_state.playlist_name)
+if 'signed_in' not in st.session_state:
+    st.session_state.signed_in = False
+
+if 'code'  not in st.session_state:
+    st.session_state.code= ""
+
+input_form= st.form(key='form', clear_on_submit=True)
+youtube_url = input_form.text_input("Enter the YouTube URL:", key="youtube_url", value= "https://www.youtube.com/watch?v=rMZeKQYLXWc")
+playlist_name= input_form.text_input("Enter the name of the playlist you want to create on Spotify:", key="playlist_name", value= "My Playlist")
+button = input_form.form_submit_button('Create my playlist!')
+
+if button:
+    if not st.session_state.signed_in:
+        st.session_state.playlist= PlaylistMaker()
+        sign_in()
         set_cached_playlist(st.session_state.playlist)
-        print('1: ', st.session_state.playlist)
-        if 'sp' not in st.session_state:
-            auth_url = st.session_state.playlist.authenticate()
-            print('authenticating...')
-            st.markdown(f'<meta http-equiv="refresh" content="0; url={auth_url}">',  unsafe_allow_html=True)
-            print('2:', st.session_state.playlist)
-            st.stop()
-    
-    print('3: ', st.session_state.playlist)
-    
-    # after redirection
-    query_params = st.query_params
-    if 'code' in query_params:
-        print('creating playlist....')
-        st.session_state.playlist = get_cached_playlist()
-        
-        if st.session_state.playlist is not None:
-            code = query_params['code']
-            if 'sp' not in st.session_state:
-                st.session_state.playlist.create_playlist(code)
-                st.write('Successfully created playlist')
-                st.session_state.sp = st.session_state.playlist.sp  # Store the authenticated session
-            else:
-                sp = st.session_state.sp
-                user_info = sp.me()
-                st.write(f"Hello {user_info['display_name']}!")            
-    else:
-        st.write('Waiting for authentication...')
 
-if __name__==  "__main__":
-    main()
+
+query_params = st.query_params
+if 'code' in query_params:
+    print('creating playlist....')
+    st.session_state.playlist = get_cached_playlist()
+
+    try: 
+        st.session_state.playlist.authorize(query_params['code'])
+    except Exception as e:
+        st.write(f'Error during authorization: {str(e)}')
+    else: 
+        st.session_state.create_playlist(st.session_state.playlist_name)
+
+    if st.session_state.playlist is not None:
+        code = query_params['code']
+        if 'sp' not in st.session_state:
+            st.session_state.playlist.create_playlist(code)
+            st.write('Successfully created playlist')
+            st.session_state.sp = st.session_state.playlist.sp  # Store the authenticated session
+        else:
+            sp = st.session_state.sp
+            user_info = sp.me()
+            st.write(f"Hello {user_info['display_name']}!")            
+else:
+    st.write('Waiting for authentication...')
+
+
 
