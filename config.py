28ea3f43e3070b4303c14c98b4cf012549136787
zatyhan <nureizzaty@gmail.com>
2024-11-28 01:37:14 +0000

    update: redis port update to work with mac

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ class BaseConfig(object):
     CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL']
     CACHE_DEFAULT_TIMEOUT = os.environ['CACHE_DEFAULT_TIMEOUT']
     SESSION_TYPE = os.environ['SESSION_TYPE']
-    SESSION_PERMANENT = os.environ['SESSION_PERMANENT']
+    PERMANENT_SESSION_LIFETIME = os.environ['PERMANENT_SESSION_LIFETIME']
     SESSION_USE_SIGNER = os.environ['SESSION_USE_SIGNER']
     SESSION_REDIS = redis.from_url(os.environ['SESSION_REDIS'])
